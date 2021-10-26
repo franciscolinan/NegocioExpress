0ec90941735e2011c61f05c3f2d62ca59065966f
franciscolinan <81978144+franciscolinan@users.noreply.github.com>
2021-10-26 18:04:21 -0300

    .

---
+++ NegocioExpress/src/NegocioExpress/Cliente.java
@@ -0,0 +1,16 @@
+package NegocioExpress;
+
+public class Cliente 
+{
+    private String nombre_;
+    
+    public Cliente(String nombre)
+    {
+        this.nombre_ = nombre;
+    }
+    
+    public String GetNombre()
+    {
+        return this.nombre_;
+    }
+}
---
+++ NegocioExpress/src/NegocioExpress/NegocioExpress.java
@@ -0,0 +1,12 @@
+package NegocioExpress;
+
+public class NegocioExpress {
+    public static void main(String[] args) 
+    {
+        // Acá comienza el código
+        // Contraseña: admin
+        Menu menu = new Menu();
+        
+        while (menu.MostrarMenu()) {}
+    }   
+}
