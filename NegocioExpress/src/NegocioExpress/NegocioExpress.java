27a0b40782528c95881b0b1e786cf0df6b72b691
franciscolinan <81978144+franciscolinan@users.noreply.github.com>
2021-10-31 19:27:33 -0300

    Update NegocioExpress.java

--- NegocioExpress/src/NegocioExpress/NegocioExpress.java
+++ NegocioExpress/src/NegocioExpress/NegocioExpress.java
@@ -3,6 +3,10 @@ package NegocioExpress;
 public class NegocioExpress {
     public static void main(String[] args) 
     {
+        // Integrantes:
+        // Francisco Liñan
+        // Adrian Sarmiento
+        
         // Acá comienza el código
         // Contraseña: admin
         Menu menu = new Menu();
